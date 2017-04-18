ac1fa511a54236f94f12e3313555a60f5812db01
Benavente, Matthew <matthew.benavente@accenture.com>
2017-04-17 21:20:59 -0500

    Added insert email trigger to EVENTS table, EventSteward Lambda

--- WIP - Unused/RSVP_Stats.js
+++ WIP - Unused/RSVP_Stats.js
@@ -1,5 +1,5 @@
+'use strict';
 var AWS = require('aws-sdk');
-
 var docClient = new AWS.DynamoDB.DocumentClient();
 var table = "EVENTS";
 
@@ -8,34 +8,35 @@ exports.handler = (event, context, callback) => {
     // console.log(JSON.stringify(event, null, 2));
     // + Iterate over every record in the batch
     event.Records.forEach(function(record) {
+        console.log('Stream record: ', JSON.stringify(record, null, 2));
         // this function only operates on new data, not modified data
-        // + Filter out all events that aren't INSERT events
-        // + Since this function modifies DynamoDB data. If it executed every time there was a modification, it would invoke itself - infinite loop - Because it would recieve events for its own modifications
-        if (record.eventName !== "INSERT") { return null; }
+
         
-        //Net Profit
-        RSVP_REVENUE = record.dynamodb.NewImage.COVER.N * record.dynamodb.NewImage.RSVP_ATTENDANCE.N;
-        RSVP_PERCENTAGE = record.dynamodb.NewImage.RSVP_ATTENDANCE.N / record.dynamodb.NewImage.ATTENDANCE.N;
+        if (record.eventName == "INSERT")  { 
+
+            var RSVP_REVENUE = record.dynamodb.NewImage.COVER.N * record.dynamodb.NewImage.RSVP_ATTENDANCE.N;
+            var RSVP_PERCENTAGE = record.dynamodb.NewImage.RSVP_ATTENDANCE.N / record.dynamodb.NewImage.ATTENDANCE.N;
         
-        //Puts back data, but now with a new calculation and timestamp
-        docClient.put({
-            TableName: table,
-            Item: {
-                "EVENT_ID": record.dynamodb.Keys.EVENT_ID.N,
-                "ATTENDANCE": record.dynamodb.NewImage.ATTENDANCE.N,
-                "RSVP_ATTENDANCE": record.dynamodb.NewImage.RSVP_ATTENDANCE.N,
-                "COVER": record.dynamodb.NewImage.COVER.N,
-                "DAY_OF_WEEK": record.dynamodb.NewImage.DAY_OF_WEEK.S,
-                "RSVP_REVENUE": RSVP_REVENUE,
-                "RSVP_PERCENTAGE": RSVP_PERCENTAGE,
-                "TIMESTAMP": (new Date()).toISOString()
-            }
-        }, function(err, data) {
-            if (err) console.log(err);
-            else console.log("DynamoDB write succeeded with: ", data);
-        });
+            //Puts back data, but now with a new calculation and timestamp
+            docClient.put({
+                TableName: table,
+                Item: {
+                    "EVENT_ID": record.dynamodb.Keys.EVENT_ID.N,
+                    "ATTENDANCE": record.dynamodb.NewImage.ATTENDANCE.N,
+                    "RSVP_ATTENDANCE": record.dynamodb.NewImage.RSVP_ATTENDANCE.N,
+                    "COVER": record.dynamodb.NewImage.COVER.N,
+                    "DAY_OF_WEEK": record.dynamodb.NewImage.DAY_OF_WEEK.S,
+                    "RSVP_REVENUE": RSVP_REVENUE,
+                    "RSVP_PERCENTAGE": RSVP_PERCENTAGE,
+                    "TIMESTAMP": (new Date()).toISOString()
+                }
+            }, function(err, data) {
+                if (err) console.log(err);
+                else console.log("DynamoDB write succeeded with: ", data);
+            });
+        }
     });
-    callback(null, {});
+    callback(null, `Successfully processed ${event.Records.length} records.`);
 };
 
 
